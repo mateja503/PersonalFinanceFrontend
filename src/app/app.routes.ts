093359fc7c69fba681f7dcd1d0a68b39a4911027
mateja503 <matejanikolikj503@gmail.com>
2025-03-17 15:01:01 +0100

    Added styling for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { BudgetComponent } from './components/body/budget/budget.component';
+import { HomeComponent } from './components/body/home/home.component';
+import { CategoryComponent } from './components/body/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        {path: '', component: HomeComponent},
+        {path:'budget',component: BudgetComponent},
+        {path: 'category',component: CategoryComponent}
+
+];
