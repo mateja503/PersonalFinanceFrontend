e17ab16ef6e2e24fec6a6342c1bebfdbe8b15986
mateja503 <matejanikolikj503@gmail.com>
2025-03-17 17:10:47 +0100

    unable to fetch data fomr the server

--- src/app/components/body/budget/budget.component.ts
+++ src/app/components/body/budget/budget.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Budget } from '../../../models/Budget';
+import { BudgetService } from '../../../services/budget.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-budget',
@@ -6,6 +11,18 @@ import { Component } from '@angular/core';
   templateUrl: './budget.component.html',
   styleUrl: './budget.component.scss'
 })
-export class BudgetComponent {
+export class BudgetComponent implements OnInit {
+
+  budgets: Budget[] = []
+
+  constructor(private budgetService: BudgetService) {}
+
+
+
+
+  ngOnInit(): void {
+  this.budgetService.getAll().subscribe((b) => this.budgets = b)
+  console.log(this.budgets)
+  }
 
 }
--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Budget } from '../models/Budget';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
+  private  apiUrl = 'https://localhost:7052/api/Budget/' 
+   constructor(private http:HttpClient) { }
 
-  constructor() { }
+   getAll():Observable<Budget[]>{
+      return this.http.get<Budget[]>(this.apiUrl+"all");
+   }
 }
